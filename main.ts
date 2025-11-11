08a165256c385edfbcb31927472ab0f551360d80
datavisorjeremychen <102835155+datavisorjeremychen@users.noreply.github.com>
2025-11-10 21:18:32 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { enableProdMode } from '@angular/core';
 import { ChatWorkbenchComponent } from './app/chat-workbench.component';
-
-// enableProdMode(); // uncomment for production
 
 bootstrapApplication(ChatWorkbenchComponent, {
   providers: [provideAnimations()]
